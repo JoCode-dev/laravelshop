e97e4d9f2a28776ce8929f39084b8c5c6931e7ee
JoCode-dev <cedricduakon@gmail.com>
2025-09-03 08:41:54 +0000

    Init: Sure! Pl

---
+++ database/migrations/2025_09_03_083509_create_orders_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use App\Enums\OrderStatus;
+use App\Models\User;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->enum('status', array_column(OrderStatus::cases(), 'value'))->default(OrderStatus::PENDING->value);
+            $table->foreignIdFor(User::class)->constrained()->cascadeOnDelete();
+            $table->float('total_amount');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('orders');
+    }
+};
