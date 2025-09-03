2c81c9af092ba5cb13b460fdaf59ba7a2c90f11f
JoCode-dev <cedricduakon@gmail.com>
2025-09-03 08:47:36 +0000

    feat: Add Laravel Debugbar and IDE Helper, enhance model relationships

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -3,8 +3,25 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Order extends Model
 {
-    //
+    protected $fillable = ['status', 'user_id', 'total_amount'];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function items(): HasMany
+    {
+        return $this->hasMany(OrderItem::class);
+    }
+
+    public function payments(): HasMany
+    {
+        return $this->hasMany(Payment::class);
+    }
 }
--- app/Models/OrderItem.php
+++ app/Models/OrderItem.php
@@ -2,9 +2,21 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class OrderItem extends Model
 {
-    //
+    protected $fillable = ['order_id', 'product_id', 'quantity', 'price'];
+
+    public function order(): BelongsTo
+    {
+        return $this->belongsTo(Order::class);
+    }
+
+    public function product(): BelongsToMany
+    {
+        return $this->belongsToMany(Product::class);
+    }
 }
